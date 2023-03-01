0a93537302f8af3aae670ba0c0f3deb9147a08c9
Daniel Miranda <85716954+Dannillouou@users.noreply.github.com>
2023-03-01 09:03:40 -0300

    Update learning python.py

--- learning python.py
+++ learning python.py
@@ -5,7 +5,13 @@ import variables
 
 #multiplos valores para multiplas variaveis
 a, b, c = 5, 3.2, 'Hello'
-
 print(a)  # prints 5
 print(b)  # prints 3.2
 print(c)  # prints Hello
+
+#printando as variaveis do arquivo de variaveis
+print(variables.PI)
+print(variables.AC_GRAVIDADE)
+print(variables.abelha)
+
+#alteracao arquivo
