1660ec78d79f16d2abb496cee80f708382db4837
Daniel Miranda <85716954+Dannillouou@users.noreply.github.com>
2023-03-01 08:29:46 -0300

    Criado learning python.py

---
+++ learning python.py
@@ -0,0 +1,11 @@
+# comentários são feitos assim
+
+#importando arquivos de variáveis
+import variables
+
+#multiplos valores para multiplas variaveis
+a, b, c = 5, 3.2, 'Hello'
+
+print(a)  # prints 5
+print(b)  # prints 3.2
+print(c)  # prints Hello
