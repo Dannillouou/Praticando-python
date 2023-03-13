d40ac0dfce1113349f51367b7494ff6c536d37cc
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-03-13 08:43:58 -0300

    operadores doidera de comparação

--- learning python.py
+++ learning python.py
@@ -158,3 +158,20 @@ print('printizinho de cria sem sep', 2023, 'muito foda', c,)
 #input
 receber = input('Digite algo: ')
 print(receber)
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#operadores doidera de comparação
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+# Olha se 'H' está presente. É case sensitive
+print('H' in x)  # prints True
+
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+
+# check if '1' key is present in y
+print(1 in y)  # prints True
+
+# check if 'a' key is present in y
+print('a' in y)  # prints False
