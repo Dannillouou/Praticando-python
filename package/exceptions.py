d777e0d5719eef71bb7a4ba83eafc2998236697f
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-04-24 09:05:34 -0300

    inicio exceptions

---
+++ package/exceptions.py
@@ -0,0 +1,10 @@
+# exceções são problemas que são identificados após a fase de análise
+# sintática do código, causados por erros lógicos, como tentar fazer
+# uma operação imposível ou abrir um arquivo que não existe, por exemplo
+
+# arquivo com exceções já inclusas no python
+print(dir(locals()['__builtins__']))
+
+# existem erros e exceções erros estão além do controle do programador
+# e não deveríamos tentar lidar com eles. Excecões podem ser pegas e
+# para que possamos lidar com elas
