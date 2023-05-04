e9e1808bfddcc2ed4dcdca27d35b86c2725b1b26
ddanieldma <danielalmemiranda@gmail.com>
2023-05-04 08:32:34 -0300

    update listas.py

--- packageListas/listas.py
+++ packageListas/listas.py
@@ -1,44 +1,47 @@
 def rodandoListas():
-    #Usando a grande de famigerada lista
+    # Usando a grande de famigerada lista
     print("Listas são sequencias de dados. Em python tanto seus elementos quanto sua ordem são mutáveis")
     linguagens = ["swift", "java", "python", "c", "html", "css", "php", "dart"]
-    #acessando
+    # acessando
     for linguagem in linguagens:
         print(linguagem)
 
-    #acesso negativo: conta a lista de tras pra frente
+    # acesso negativo: conta a lista de tras pra frente
     print(linguagens[-2])
 
-    #cortando lista
+    # cortando lista
     print(linguagens[3:6])
-    #começando do fim
+    # começando do fim
     print(linguagens[:])
 
-    #adicionando elementos
+    # adicionando elementos
     linguagens.append("c++")
     print(linguagens)
 
-    #adicionando toda a lista a outra
+    # concatenando listas
+    print(frameworks + linguagens)
+    
+    # adicionando toda a lista a outra
     frameworks = ["cake", "flutter", "bootsrap"]
     print(frameworks)
     frameworks.extend(linguagens)
     print(frameworks)
 
-    #mudando valor de lista
-    linguagens[-1] = "C#"
+    # mudando valor de lista
+    linguagens[-1] = "C# "
     print(linguagens)
 
-    #removendo itens
-    del linguagens[-1] #remove o último
+    # removendo itens
+    del linguagens[-1] # remove o último
     linguagens.remove('swift')
     print(linguagens)
 
-    #vendo se elemento existe na lista
-    print('C#' in linguagens)
+    # vendo se elemento existe na lista
+    print('C# ' in linguagens)
     print('python' in linguagens)
 
-    #tamanho da lista
+    # tamanho da lista
     print('Tamanho da lista: ', len(linguagens))
 
-    #printando tipo da lista
+    # printando tipo da lista
     print(type(linguagens))
