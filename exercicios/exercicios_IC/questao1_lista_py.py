8c8e1a99fbfa0c7737b5a2dc0e8b74258394a2df
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-05-05 11:43:52 -0300

    exercicio 1

--- exercicios/exercicios_IC/questao1_lista_py.py
+++ exercicios/exercicios_IC/questao1_lista_py.py
@@ -1,7 +1,12 @@
+# nome de filme de exmeplo
 branca = "branca/de/neve"
 lista = list(branca)
-print(lista)
+# print(lista)
+
+# juntando toda a lista em uma string sem espaços
 lista_nova = ''.join(lista)
-print(type(lista_nova))
+# print(type(lista_nova))
+
+# trocando barras por espaços
 final = lista_nova.replace('/', ' ')
-print(final)
+# print(final)
