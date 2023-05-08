6ed7a1c9cfcdd9a40fb8f65e5b63b1f5c0f5db6e
ddanieldma <danielalmemiranda@gmail.com>
2023-05-08 08:56:27 -0300

    geradores

--- packages/geradores.py
+++ packages/geradores.py
@@ -30,6 +30,28 @@ gerado = gerador(10)
 print(next(gerado)) # printa 0
 print(next(gerado)) # printa 1
 
+# gerador de potencias em uma linha
+gerador_potencia = (num * num for num in range(20))
+
 print("Dentro do for:")
-for valor in gerado:
-    print(valor) # printa de 2 a 9
+for valor in gerador_potencia:
+    print(valor) # printa de 0 a 19^2
+
+# definindo a mesma ideia da classe iteradora de potencias de dois, 
+# mas com gerador
+
+# 
+def Exponencial(base = 2, expoente_maximo = 0):
+    expoente = 0
+    while expoente < expoente_maximo:
+        yield base ** expoente
+        expoente += 1
+
+funcao_exponencial = Exponencial(3, 10)
+
+for valor in funcao_exponencial:
+    print(valor)
+
+# funções geradoras são mais eficientes na memória, pois
+# são um iterável que entrega um número de cada vez, sem precisar
+# 
