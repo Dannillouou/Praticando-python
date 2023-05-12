5c535586a04630c1c9ecdb9222917d806674400c
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-05-12 12:52:46 -0300

    usando numpy

---
+++ packages/usando_numpy.py
@@ -0,0 +1,8 @@
+#numpy permite ter uma forma de trabalhar com lista como se fossem matrizes
+
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5])
+
+print(a) # printa a matriz
+print(a.shape) # printa o que seria o shape da matriz ou o tamanho de certa forma
