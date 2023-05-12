c53cbaeeae1a704ff8db165ff34102eab9511302
Daniel Miranda <85716954+ddanieldma@users.noreply.github.com>
2023-05-12 11:46:55 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-def fibonacci(n):
+import math
 
-   temp1, temp2 = 0, 1
-
-   total = 0
-
-   while total < n:
-
-       yield temp1
-
-       temp3 = temp1 + temp2
-
-       temp1 = temp2
-
-       temp2 = temp3
-
-       total += 1
-
-fib_object = fibonacci(20)
-
-for i in fib_object:
-    print(i)
-
-print(list(fib_object))
-
-dictionary = {key:values for key,values in zip(range(9), 'guilherme')}
-print(dictionary)
-print(dictionary.keys())
+print(math)
