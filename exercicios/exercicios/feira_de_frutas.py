a98d37044e7a721730a2f52d9760ea4a65f73466
ddanieldma <danielalmemiranda@gmail.com>
2023-05-14 13:15:29 -0300

    feira de frutas feita, mas meio mal feita

--- exercicios/exercicios/feira_de_frutas.py
+++ exercicios/exercicios/feira_de_frutas.py
@@ -26,12 +26,23 @@ def preco_maca(quantidade_kg) -> float:
 def tem_desconto(quantidade_kg, preco):
     if quantidade_kg > 8 or preco > 25:
         return True
+    else:
+        return False
+    
+def preco_total(preco_morangos, preco_macas):
+    return preco_morangos + preco_macas
+
+def preco_final(preco_total, tem_desconto) -> float:
+    if tem_desconto:
+        return preco_total - preco_total/10
+    else:
+        preco_total
 
 def recebendo_quantidade(fruta) -> int:
     valor_invalido = True
     while valor_invalido:
         try:
-            quantidade_fruta = int(input(f"Quanto voce vai querer de {fruta} (kg)?"))
+            quantidade_fruta = int(input(f"\nQuanto voce vai querer de {fruta} (kg)? "))
             valor_invalido = False
         except ValueError:
             print("Input invalido, tente novamente")
@@ -39,16 +50,35 @@ def recebendo_quantidade(fruta) -> int:
 
     return quantidade_fruta
 
-print("Preços das frutas:")
-print("Morango:")
-print("até 5 kg: R$ 2,50/kg")
-print("mais de 5 kg: R$ 2,20/kg")
-print("Maçã:")
-print("até 5 kg: R$ 1,80/kg")
-print("mais de 5 kg: R$ 1,50/kg")
-print("Você comprando mais de 8kg ou 25 reais, ainda tem um desconto de 10%")
+def menu_inicial():
+    print("Preços das frutas:")
+    print("Morango:")
+    print("até 5 kg: R$ 2,50/kg")
+    print("mais de 5 kg: R$ 2,20/kg")
+    print("Maçã:")
+    print("até 5 kg: R$ 1,80/kg")
+    print("mais de 5 kg: R$ 1,50/kg")
+    print("Você comprando mais de 8kg ou 25 reais, ainda tem um desconto de 10%")
 
+#============================================================================================
+#programa sendo rodado
+
+#printando menu inicial
+menu_inicial()
 # recebendo quantidade de morangos
 quantidade_morangos = recebendo_quantidade("Morango")
 # recebendo quantidade de maçãs
 quantidade_macas = recebendo_quantidade("Maca")
+# quantidade total
+quantidade_total = quantidade_morangos + quantidade_macas
+
+preco_total = preco_total(preco_morango(quantidade_morangos), preco_maca(quantidade_macas))
+tem_desconto = tem_desconto(quantidade_total, preco_total)
+preco_final = preco_final(preco_total, tem_desconto)
+
+if tem_desconto:
+    print("\nVoce recebeu um desconto!:")
+    print(f"O valor caiu de: R$ {preco_total}")
+    print(f"Para: R$ {preco_final}")
+else:
+    print("O valor da sua compra é de: ", preco_final)
