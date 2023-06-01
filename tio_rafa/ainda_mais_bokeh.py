1aae18479a6f6e84913fa87a951f834f306db544
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-06-01 08:19:45 -0300

    ajustando toolbar

--- tio_rafa/ainda_mais_bokeh.py
+++ tio_rafa/ainda_mais_bokeh.py
@@ -17,6 +17,17 @@ plot.toolbar.autohide = True
 # mudando localização da toolbar
 plot.toolbar_location = "below"
 
-plot.circle([1,2,3,4,5], [2,4,6,8,10])
+plot.circle([1,2,3,4,5], [2,4,6,8,10], 
+            legend_label="Sei la")# adicionando legenda à esse scatter plot
+
+# ajustando legenda
+plot.legend.location = "top"
+plot.legend.location = "top_left"
+plot.legend.background_fill_alpha = 0
+plot.legend.border_line_color = "#ffbca4"
+# plot.legend.border_radius = 5
+plot.legend.label_text_color = "#ff99ff"
+plot.legend.label_text_font = "times"
+plot.legend.label_text_font_size = "45px"
 
 show(plot)
