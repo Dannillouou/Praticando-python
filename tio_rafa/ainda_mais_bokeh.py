7959f31f82624712edf2a832ba733c44995d6feb
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-06-01 08:31:01 -0300

    mexendo com o renderizador

--- tio_rafa/ainda_mais_bokeh.py
+++ tio_rafa/ainda_mais_bokeh.py
@@ -17,7 +17,7 @@ plot.toolbar.autohide = True
 # mudando localização da toolbar
 plot.toolbar_location = "below"
 
-plot.circle([1,2,3,4,5], [2,4,6,8,10], 
+renderer = plot.circle([1,2,3,4,5], [2,4,6,8,10], 
             legend_label="Sei la")# adicionando legenda à esse scatter plot
 
 # ajustando legenda
@@ -29,5 +29,15 @@ plot.legend.border_line_color = "#ffbca4"
 plot.legend.label_text_color = "#ff99ff"
 plot.legend.label_text_font = "times"
 plot.legend.label_text_font_size = "45px"
+
+# mudando renderizador dos pontos
+glyph_renderer = renderer.glyph
+
+# ajustando renderizador
+glyph_renderer.size = 42
+glyph_renderer.fill_alpha = 0.5
+glyph_renderer.line_color = "#dfc76c"
+glyph_renderer.line_width = 2
+glyph_renderer.line_dash = [7,3]
 
 show(plot)
