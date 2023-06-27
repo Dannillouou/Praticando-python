54a932624cf2b03a9d86bb351fd050e5d3f977a2
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-06-27 15:28:47 -0300

    organizando repositorio

--- tio_rafa/main.py
+++
@@ -1,6 +0,0 @@
-from modulo_criptografas import *
-
-print(cifra_reversa("Eu adoro a EMAp"))
-mensagem_criptografada = cifra_de_cesar("Eu adoro a EMAp", "encriptar", 3)
-print(mensagem_criptografada)
-print(cifra_de_cesar(mensagem_criptografada, "decriptar", 3))
